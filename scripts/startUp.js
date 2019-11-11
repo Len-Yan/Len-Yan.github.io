d6a0f3c8f7f76437174e476f8a7d4470733920b2
Chris Hundhausen <chundhau@gmail.com>
2019-11-11 13:48:33 -0800

    Initial commit

---
+++ scripts/startUp.js
@@ -0,0 +1,6 @@
+//Start-up functions run when page is loaded.
+includeHTML();
+document.getElementById("profileDiv").style.display = "none";
+document.getElementById("roundsDiv").style.display = "none";
+document.getElementById("coursesDiv").style.display = "none";
+document.getElementById("logRoundDiv").style.display = "none";
